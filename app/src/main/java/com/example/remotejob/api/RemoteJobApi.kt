392b789bae2628ca2c5235444376a1fbcffc6e56
mgr-mirnal <mgr.mirnal97@gmail.com>
2022-06-13 12:01:03 +0100

    Working Search page

--- app/src/main/java/com/example/remotejob/api/RemoteJobApi.kt
+++ app/src/main/java/com/example/remotejob/api/RemoteJobApi.kt
@@ -4,13 +4,17 @@ package com.bersyte.remojob.api
 import com.example.remotejob.models.RemoteJobResponse
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 
 interface RemoteJobApi {
 
-    @GET("remote-jobs?limit=5")
+    @GET("remote-jobs?limit=10")
     fun getRemoteJobResponse(): Call<RemoteJobResponse>
 
-
+    @GET("remote-jobs")
+    fun searchJob(
+        @Query("search") search : String?
+    ): Call<RemoteJobResponse>
 
 }
